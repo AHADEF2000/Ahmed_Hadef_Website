1a7c7c7e8629d895660a9e233cd045b296b94241
AhmedHadef <ahadef130820@gmail.com>
2023-05-12 00:45:27 +0300

    I added a new project to the portfolio

--- src/Components/projects/JavaScript.jsx
+++ src/Components/projects/JavaScript.jsx
@@ -1,7 +1,7 @@
 import todoImg from "../../assets/Imgs/todo-app.png"
 import quizImg from "../../assets/Imgs/Quiz_app.png"
 import hangImg from "../../assets/Imgs/hang_man_game.png"
-
+import Exchange from "../../assets/Imgs/Exchange.png"
 
 function JavaScript() {
     return (
@@ -32,7 +32,16 @@ function JavaScript() {
                     <div className="project-info">
                         <h3>Hang Man Game</h3>
                         <p> More Details  <i className="uil uil-arrow-right"></i></p>
+                    </div>
+                </a>
+            </div>
 
+            <div className="project-list-item">
+                <a href="https://ahadef2000.github.io/Exchange_app_practice/" target="blank">
+                    <img src={Exchange} />
+                    <div className="project-info">
+                        <h3>Exchange App</h3>
+                        <p> More Details  <i className="uil uil-arrow-right"></i></p>
                     </div>
                 </a>
             </div>
