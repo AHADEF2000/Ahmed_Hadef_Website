631646656f38457c58f228640b11bac371ac5e2a
Ahmed Hadef <ahadef130820@gmail.com>
2023-05-26 04:36:41 +0300

    updated the cv info

--- src/Components/about/Info.jsx
+++ src/Components/about/Info.jsx
@@ -1,4 +1,4 @@
-import CV from "../../assets/docs/Ahmed_Hadef_resume.pdf";
+import resume from "../../assets/docs/Ahmed_Hadef_resume.pdf";
 
 function Info() {
 
@@ -6,7 +6,7 @@ function Info() {
         <>
             <div className="about-info">
                 <p>Front End Developer, I create web pages and am very passionate about it. I create the right websites for your business.</p>
-                <button type="button" id="btn"> <a href={CV} download='ahmed_cv'> Download CV <i className="fa-regular fa-file-lines"></i></a></button>
+                <button type="button" id="btn"> <a href={resume} download='ahmed_cv'> Download CV <i className="fa-regular fa-file-lines"></i></a></button>
             </div>
         </>
     )
