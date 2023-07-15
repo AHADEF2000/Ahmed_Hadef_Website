e266a0cad2411149f061863136a16ac01cae68b8
Ahmed Hadef <ahadef130820@gmail.com>
2023-07-16 01:34:01 +0300

    I uppdated my cv

--- src/Components/about/Info.jsx
+++ src/Components/about/Info.jsx
@@ -1,15 +1,21 @@
 import resume from "../../assets/docs/Ahmed_CV.pdf";
 
 function Info() {
-
-    return (
-        <>
-            <div className="about-info">
-                <p>Front End Developer, I create web pages and am very passionate about it. I create the right websites for your business.</p>
-                <button type="button" id="btn"> <a href={resume} download='ahmed_cv'> Download CV <i className="fa-regular fa-file-lines"></i></a></button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="about-info">
+        <p>
+          Front End Developer, I create web pages and am very passionate about
+          it. I create the right websites for your business.
+        </p>
+        <button type="button" id="btn">
+          <a href={resume} download="ahmed_cv">
+            Download CV <i className="fa-regular fa-file-lines"></i>
+          </a>
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Info
+export default Info;
