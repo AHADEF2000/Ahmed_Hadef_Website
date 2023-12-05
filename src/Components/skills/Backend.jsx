3cd7cd5aff4443d3bfdae6b7344ce3de56a73913
Ahmed Hadef <ahadef130820@gmail.com>
2023-12-05 19:35:16 +0300

    updated the skills

--- src/Components/skills/Backend.jsx
+++ src/Components/skills/Backend.jsx
@@ -1,47 +1,61 @@
 function Other() {
-    return (
-        <>
-            <div className="skill-box">
-                <h2>Backend</h2>
-                <div className="skill-contnet">
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <i className="fa-solid fa-circle-check"></i>
-                        </div>
-                        <div className="skill-info">
-                            <h3>Node.js</h3>
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <i className="fa-solid fa-circle-check"></i>
-                        </div>
-                        <div className="skill-info">
-                            <h3>Express JS</h3>
-
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <i className="fa-solid fa-circle-check"></i>
-                        </div>
-                        <div className="skill-info">
-                            <h3>PostgresSQL</h3>
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill-icon">
-                            <i className="fa-solid fa-circle-check"></i>
-                        </div>
-                        <div className="skill-info">
-                            <h3>MongoDB</h3>
-
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="skill-box">
+        <h2>Backend</h2>
+        <div className="skill-contnet">
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
             </div>
-        </>
-    )
+            <div className="skill-info">
+              <h3>Node.js</h3>
+            </div>
+          </div>
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
+            </div>
+            <div className="skill-info">
+              <h3>Express JS</h3>
+            </div>
+          </div>
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
+            </div>
+            <div className="skill-info">
+              <h3>PostgresSQL</h3>
+            </div>
+          </div>
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
+            </div>
+            <div className="skill-info">
+              <h3>MongoDB</h3>
+            </div>
+          </div>
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
+            </div>
+            <div className="skill-info">
+              <h3>Python</h3>
+            </div>
+          </div>
+          <div className="skill">
+            <div className="skill-icon">
+              <i className="fa-solid fa-circle-check"></i>
+            </div>
+            <div className="skill-info">
+              <h3>Django</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Other
+export default Other;
