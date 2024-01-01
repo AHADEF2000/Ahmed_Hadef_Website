3942914cd63296eea0f9c1897c042a8b6d2b6e57
Ahmed Hadef <ahadef130820@gmail.com>
2024-01-01 22:31:36 +0300

    update my cv

--- src/Components/about/Info.jsx
+++ src/Components/about/Info.jsx
@@ -1,12 +1,12 @@
-import resume from "../../assets/docs/Ahmed_Hadef_Resume.pdf";
+import resume from "../../assets/docs/Ahmed-Hadef_Resume.pdf";
 
 function Info() {
   return (
     <>
       <div className="about-info">
         <p>
-          Front End Developer, I create web pages and am very passionate about
-          it. I create the right websites for your business.
+          Web Developer, I create web pages and am very passionate about it. I
+          create the right websites for your business.
         </p>
         <button type="button" id="btn">
           <a href={resume} download="ahmed_cv">
